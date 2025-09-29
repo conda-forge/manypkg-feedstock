3e7bf7819904de5f43dd1807c1b4f25b85f2e9fe
Daniel Nachun <daniel.nachun@gmail.com>
2025-09-29 12:06:30 -0700

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
+    --no-bin-links \
     --global \
     --build-from-source \
     ${SRC_DIR}/${PKG_NAME}-cli-${PKG_VERSION}.tgz
@@ -13,6 +14,13 @@ npm install -ddd \
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
+mkdir -p ${PREFIX}/bin
+tee ${PREFIX}/bin/manypkg << EOF
+#!/bin/sh
+exec \${CONDA_PREFIX}/lib/node_modules/@manypkg/cli/bin.js "\$@"
+EOF
+chmod +x ${PREFIX}/bin/manypkg
+
 tee ${PREFIX}/bin/manypkg.cmd << EOF
-call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\manypkg %*
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\lib\node_modules\@manypkg\cli\bin.js %*
 EOF
